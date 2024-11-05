3a87ffc90999326c09ae34fad2d7f02c0191d067
Yasharth <look4yasharth@gmail.com>
2024-11-05 09:30:36 +0530

    added Pattern1.java

---
+++ 5- Competitive Programming/Java/Patterns/Problems/Pattern1.java
@@ -0,0 +1,35 @@
+/*
+
+Write a Java Program to print the following pattern:
+
+****
+****
+****
+****
+
+Take number of rows as input from user.
+
+*/
+
+import java.util.Scanner;
+
+public class Pattern1
+{
+	public static void main(String args[])
+	{
+		int row = 1, column, numberOfRows;
+		Scanner sc = new Scanner(System.in);
+		numberOfRows = sc.nextInt();
+		while(row <= numberOfRows)
+		{
+			column = 1;
+			while(column <= numberOfRows)
+			{
+				System.out.print("*");
+				column++;
+			}
+			System.out.print("\n");
+			row++;
+		}
+	}
+}
