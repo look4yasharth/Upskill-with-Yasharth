dd0ed715cf6daf5624c3cc7faea1a3f0c3e9d66c
Yasharth <look4yasharth@gmail.com>
2024-11-20 22:07:39 +0530

    added Pattern15.java

---
+++ 5- Competitive Programming/Java/Patterns/Problems/Pattern15.java
@@ -0,0 +1,34 @@
+/*
+
+Write a Java Program to print the following pattern:
+
+A
+BB
+CCC
+
+Take number of rows as input from user.
+
+*/
+
+import java.util.Scanner;
+
+public class Pattern15
+{
+	public static void main(String args[])
+	{
+		int row = 1, column, numberOfRows;
+		Scanner sc = new Scanner(System.in);
+		numberOfRows = sc.nextInt();
+		while(row <= numberOfRows)
+		{
+			column = 1;
+			while(column <= row)
+			{
+				System.out.print((char)(64 + row));
+				column++;
+			}
+			System.out.print("\n");
+			row++;
+		}
+	}
+}
