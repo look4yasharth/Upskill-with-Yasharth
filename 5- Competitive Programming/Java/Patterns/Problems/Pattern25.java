c52059a6f7802a8843e825f4b20343b4466ab72b
Yasharth <look4yasharth@gmail.com>
2024-11-25 16:19:12 +0530

    added Pattern25.java

---
+++ 5- Competitive Programming/Java/Patterns/Problems/Pattern25.java
@@ -0,0 +1,46 @@
+/*
+
+Write a Java Program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+
+Take number of rows as input from user.
+
+*/
+
+import java.util.Scanner;
+
+public class Pattern25
+{
+	public static void main(String args[])
+	{
+		int row = 1, column, numberOfRows;
+		Scanner sc = new Scanner(System.in);
+		numberOfRows = sc.nextInt();
+		while(row <= numberOfRows)
+		{
+			column = 1;
+			while(column <= numberOfRows - row)
+			{
+				System.out.print(" ");
+				column++;
+			}
+			while(column <= numberOfRows)
+			{
+				System.out.print("*");
+				column++;
+			}
+			column = 1;
+			while(column <= row - 1)
+			{
+				System.out.print("*");
+				column++;
+			}
+			System.out.print("\n");
+			row++;
+		}
+	}
+}
